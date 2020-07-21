78bc4149de5db2146641a9a7bf8664c169b45cf4
susanka068 <sathinandimajumder068@gmail.com>
2020-07-22 02:01:57 +0530

    homepage updated

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Navbar , Nav , FormControl , Button , Form , Image } from 'react-bootstrap'
 import Logo from '../Img/logos/favicon-32x32.png'
+import {Link} from 'react-router-dom'
 export default function HomePage() {
     return (
 <Navbar bg="light" expand="lg">
@@ -8,8 +9,8 @@ export default function HomePage() {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/products">Explore Products</Nav.Link>
+      <Link to="/"><Nav.Link href="/">Home</Nav.Link></Link>
+      <Link to='/products'><Nav.Link href="/products">Explore Products</Nav.Link></Link>
       <Nav.Link href="#">Extract</Nav.Link>
     </Nav>
     <Form inline>
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,15 +1,55 @@
 import React from 'react'
-import {Jumbotron , Button } from 'react-bootstrap'
+import {Jumbotron , Button , Image ,Card } from 'react-bootstrap'
+import Logo from '../Img/logos/android-chrome-512x512.png'
 export default function HomePage() {
     return (
-        <Jumbotron>
-        <h1>Diana</h1>
-        <p>
-            Diana is an Fashion Intelligent System for designers and retailers powered by modern tool of machine learning 
-        </p>
-        <p>
-            <Button variant="primary">Learn more</Button>
-        </p>
+        <div style={{'display':'flex' , 'flexDirection':'column' ,'flexWrap':'wrap'}}>
+        <Jumbotron style={{'backgroundColor' : 'white' , 'padding' : "100px 20px" , 'position' : 'relative'}} >
+        <div className="container">
+            <div className="row" >
+                <div className="col-5" style={{left : '-8%' , 'width' :'auto'}}>
+                    <h1>Diana</h1>
+                    <h3>Fashion Intelligent System</h3> 
+                    <p>
+                        Diana is an Fashion Intelligent System for designers and retailers powered by modern tool of machine learning
+                        sjdkdsfnsfnsdjkdsbd  asnshsd sdhdsfbsd sbdsb sdnsb sdhsd  bjsfnbsd bsbv vsd bsdj sjdkd hjhe sdbjsd dsbsdf sbsd bjsfnb sdbsfb bsdbds dsbds 
+                    </p>
+                    <p>
+                        <Button variant="primary">Learn more</Button>
+                    </p>
+                </div>
+                <div class="col-7">
+                    <Image src={Logo} style={{'height':'400px' , 'width': '400px' , position: 'relative'  }} />
+                </div>
+            </div>
+        </div>
         </Jumbotron>
+        <div className='row' style={{position :'relative'}}>
+        <div className="col-6"  style={{left : '15%'}} >
+        <Card style={{ width: '18rem' }}>
+        <Card.Body>
+            <Card.Title>GO EXTRACTING</Card.Title>
+            <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+            </Card.Text>
+            <Button variant="primary">Extract</Button>
+        </Card.Body>
+        </Card>
+        </div>
+        <div className="col-6" >
+        <Card style={{ width: '18rem' }}>
+        <Card.Body>
+            <Card.Title>GO EXPLORING</Card.Title>
+            <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+            </Card.Text>
+            <Button variant="primary">EXPLORE</Button>
+        </Card.Body>
+        </Card>
+        </div>
+        </div>
+        </div>
     )
 }
