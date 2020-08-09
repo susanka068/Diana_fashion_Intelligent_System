85e1274730e5d78c5692b68904f6df3a65e62c85
susanka068 <sathinandimajumder068@gmail.com>
2020-08-09 16:46:52 +0530

    final

--- src/Component/ProductIndividual.js
+++ src/Component/ProductIndividual.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react'
 import {Card ,Button , Accordion , Table } from 'react-bootstrap'
 export class ProductIndividual extends Component {
     render() {
-        const { title , image ,score } = this.props.data
+        const { title , image ,score , detail } = this.props.data
+        const detail_string = JSON.stringify(detail) 
         return (
             <Accordion>
             <Card>
                 <Card.Header>
                 <Card.Img src={image} style={{height : 'auto' , width: '140px'}}  ></Card.Img>
+                trendiness score : {score}
                 <Card.Title>{title}</Card.Title>
                 <Accordion.Toggle as={Button} variant="link" eventKey="0">
                     <Button className="btn btn-primary mybtn" variant="primary">view details</Button>
                 </Accordion.Toggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey="0">
-        <Card.Body>trendiness score : {score} </Card.Body>
+                <Card.Body>{JSON.stringify(this.props.data)}</Card.Body>
                 </Accordion.Collapse>
             </Card>
             </Accordion>
